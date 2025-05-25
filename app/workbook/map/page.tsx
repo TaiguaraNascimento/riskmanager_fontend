dd77759b58e08174da39d2f1b45c47faac9578b3
Taiguara Nascimento <taiguara.nascimento@gmail.com>
2025-05-25 17:19:15 -0300

    Establish

---
+++ app/workbook/map/page.tsx
@@ -0,0 +1,9 @@
+import ProcessMapping from "@/components/process-mapping";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <ProcessMapping />
+    </main>
+  );
+}
--- app/workbook/page.tsx
+++ app/workbook/page.tsx
@@ -1,3 +1,5 @@
+import ProcessMapping from "@/components/process-mapping";
+
 export default function Home() {
   return <main className="min-h-screen bg-white">x</main>;
 }
